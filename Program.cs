d15492e036fbf0631657c14d63864a297acd66d5
murillojoaquim <murillojalves@hotmail.com>
2021-08-29 20:04:39 -0300

    Arte adicionada

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" ||  ||");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" \\\\()// ");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("//(__)\\\\");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("||    ||");
+
+            Console.ResetColor();
+        }
+    }
+}
